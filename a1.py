dfaf1cf3347442405504d8de6ce3720481b2e939
cw <christopherwilliamanglin@gmail.com>
2023-11-10 14:27:56 -0500

    added 1b (abalone categorization of Type)

--- a1.py
+++ a1.py
@@ -11,14 +11,25 @@ import matplotlib
 penguins = pd.read_csv('penguins.csv')
 abalone = pd.read_csv('abalone.csv')
 
+# do the one-hot encoding for penguins
 penguins_oneHot = pd.get_dummies(penguins, columns=['island','sex'])
 
-penguins_manual=penguins
-penguins_manual['island']=pd.Categorical(penguins_manual['island'])
-penguins_manual['sex']=pd.Categorical(penguins_manual['sex'])
+# do the manual categorization for penguins
+penguins_manual = penguins
+penguins_manual['island'] = pd.Categorical(penguins_manual['island'])
+penguins_manual['sex'] = pd.Categorical(penguins_manual['sex'])
 
-penguins_manual['island']=penguins_manual['island'].cat.codes
-penguins_manual['sex']=penguins_manual['sex'].cat.codes
+penguins_manual['island'] = penguins_manual['island'].cat.codes
+penguins_manual['sex'] = penguins_manual['sex'].cat.codes
+
+# do the catergorization for abalone (looks like Type needs to be made numerical)
+abalone_categorized = abalone
+
+abalone_categorized['Type'] = pd.Categorical(abalone_categorized['Type']) # make string categorical
+
+abalone_categorized['island']  = abalone_categorized['island'].cat.codes # make values numerical
+
+
 
 
 
