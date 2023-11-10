9595fdc7bb580ac520e324474c12f2def5a9e682
danieleComitogianni <116045112+danieleComitogianni@users.noreply.github.com>
2023-11-10 14:24:01 -0500

    did 1a

--- a1.py
+++ a1.py
@@ -11,6 +11,15 @@ import matplotlib
 penguins = pd.read_csv('penguins.csv')
 abalone = pd.read_csv('abalone.csv')
 
+penguins_oneHot = pd.get_dummies(penguins, columns=['island','sex'])
+
+penguins_manual=penguins
+penguins_manual['island']=pd.Categorical(penguins_manual['island'])
+penguins_manual['sex']=pd.Categorical(penguins_manual['sex'])
+
+penguins_manual['island']=penguins_manual['island'].cat.codes
+penguins_manual['sex']=penguins_manual['sex'].cat.codes
+
 
 
 
