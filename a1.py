c92d66bed039ca91b0112ae00b6e7cb1fe35ce33
cw <christopherwilliamanglin@gmail.com>
2023-11-10 15:52:33 -0500

    back up to date

--- a1.py
+++ a1.py
@@ -1,16 +1,16 @@
 
+
 # A1 COMP472
 
 # TEAM MEMBERS:
 
 import pandas as pd
 import sklearn
-import matplotlib
-
+import matplotlib.pyplot as plt
 
 # read the .csv files in
-penguins = pd.read_csv('COMP472_A1/penguins.csv')
-abalone = pd.read_csv('COMP472_A1/abalone.csv')
+penguins = pd.read_csv('penguins.csv')
+abalone = pd.read_csv('abalone.csv')
 
 # do the one-hot encoding for penguins
 penguins_onehot = pd.get_dummies(penguins, columns=['island','sex'])
@@ -35,24 +35,45 @@ penguins_onehot.to_csv('penguins_onehot.csv')
 penguins_manual.to_csv('penguins_manual.csv')
 abalone_categorized.to_csv('abelone_categorized.csv')
 
+# plotting the species percentage distribution for penguins
+penguins_species_counts = penguins_onehot['species'].value_counts(normalize=True)*100
+plt.figure()
+penguins_species_counts.plot(kind='bar')
+plt.title("Penguin Species")
+plt.xlabel('Species')
+plt.ylabel('Percentages')
+plt.savefig('penguin-classes.png')
+
+# plotting the sex percentage distribution for abalone
+abalone_sex_counts = abalone_categorized['Type'].value_counts(normalize=True)*100
+plt.figure()
+abalone_sex_counts.plot(kind='bar')
+plt.title("Abalone Sex")
+plt.xlabel('Sex')
+plt.xticks(ticks=[0,1,2],labels=['Female','Infant','Male'])
+plt.ylabel('Percentages')
+plt.savefig('abalone-classes.png')
+
 # split datasets up for training and test:
 # penguin 
-penguin_features = penguins_onehot.drop('species')  
+penguin_features = penguins_onehot.drop('species', axis = 1)  
 penguin_labels = penguins_onehot['species']
 
 xtrain_penguin, xtest_penguin, ytrain_penguin, ytest_penguin = sklearn.model_selection.train_test_split(penguin_features, penguin_labels)
 
 # abalone
-abalone_features = abalone_categorized.drop('sex')  
+abalone_features = abalone_categorized.drop('sex', axis = 1)  
 abalone_labels = abalone_categorized['sex']
 
 xtrain_abalone, xtest_abalone, ytrain_abalone, ytest_abalone = sklearn.model_selection.train_test_split(abalone_features, abalone_labels)
 
+# (4a) base-DT:
+# create the decision tree classifier
+decision_tree_classifier_penguins = sklearn.tree.DecisionTreeClassifier()
+# fit the training data :)
+decision_tree_classifier_penguins.fit(xtrain_penguin, ytrain_penguin)
 
-
-
-
-
-
-
-
+# plot the tree
+plt.plot_tree(decision_tree_classifier_penguins, feature_names = xtrain_penguin.columns)
+plt.savefig('penguin_basicDT.png')
+plt.show()
