5d0219cbb8c8c0ae61bfe0ac37ca3c66512176bf
Invidiuz <stefanvcodrean@gmail.com>
2023-11-10 17:11:26 -0500

    Fixed dan's push

--- a1.py
+++ a1.py
@@ -1,15 +1,20 @@
 # A1 COMP472
 
 # TEAM MEMBERS:
+# Chris Anglin 40216346
+# Stefan Codrean
+# Daniele Comitogianni
 
 import pandas as pd
 import sklearn
 import sklearn.model_selection as ms
 import sklearn.tree as tree
 import matplotlib.pyplot as plt
-from sklearn.neural_network import MLPClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn import tree
 
-
+# 1.
 # read the .csv files in
 penguins = pd.read_csv('penguins.csv')
 abalone = pd.read_csv('abalone.csv')
@@ -37,6 +42,7 @@ penguins_onehot.to_csv('penguins_onehot.csv')
 penguins_manual.to_csv('penguins_manual.csv')
 abalone_categorized.to_csv('abelone_categorized.csv')
 
+# 2.
 # plotting the species percentage distribution for penguins
 penguins_species_counts = penguins_onehot['species'].value_counts(normalize=True)*100
 plt.figure()
@@ -56,6 +62,7 @@ plt.xticks(ticks=[0,1,2],labels=['Female','Infant','Male'])
 plt.ylabel('Percentages')
 plt.savefig('abalone-classes.png')
 
+# 3.
 # split datasets up for training and test:
 # penguin 
 
@@ -71,7 +78,8 @@ abalone_labels = abalone_categorized['Type']
 
 xtrain_abalone, xtest_abalone, ytrain_abalone, ytest_abalone = ms.train_test_split(abalone_features, abalone_labels)
 
-# (4a) base-DT:
+# 4.
+# base-DT:
 # penguin
 # create the decision tree classifier
 decision_tree_classifier_penguins = tree.DecisionTreeClassifier()
@@ -81,20 +89,28 @@ decision_tree_classifier_penguins.fit(xtrain_penguin, ytrain_penguin)
 # plot the tree
 tree.plot_tree(decision_tree_classifier_penguins, feature_names = xtrain_penguin.columns)
 plt.savefig('penguin_basicDT.png')
-plt.show()
 
+#(4b) Top-DT
 
-# #BASE-MLP
-# mlp = MLPClassifier(hidden_layer_sizes=(100, 100), activation = 'logistic', solver = 'sgd')
-# mlp.fit(xtrain_abalone, ytrain_abalone)
-# prediction = mlp.predict(xtest_abalone)
+#setting up parameter Grid
+parameter_grid={
+    'criterion': ['gini', 'entropy'],
+    'max_depth': [None, 10, 20], #just using 10, 20 to see the difference in values
+    'min_samples_split':[5, 10, 20]
+}
 
-# abalone
-# create a decision tree classifier
-decision_tree_classifier_abalone = tree.DecisionTreeClassifier()
-# fit the training data :)
-decision_tree_classifier_abalone.fit(xtrain_abalone, ytrain_abalone)
+#initializes the decision tree classifier
+dt = DecisionTreeClassifier()
 
-# plot the tree (i tried some different max depths to get a legible looking tree, kind of cool just to see the full tree though)
-tree.plot_tree(decision_tree_classifier_abalone, feature_names = xtrain_abalone.columns)
-plt.savefig('abalone_basicDT.png')
+grid_search = GridSearchCV(dt, parameter_grid, cv=5, scoring = 'accuracy')
+
+grid_search.fit(xtrain_penguin, ytrain_penguin)
+
+best_tree = grid_search.best_estimator_
+
+plt.figure()
+tree.plot_tree(best_tree, filled=True)
+
+plt.savefig('best_decision_tree.png')
+plt.close()
+
